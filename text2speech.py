dea61fb7d758d3dc31bde378f1795df2f97944d8
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2022-12-24 18:37:56 -0500

    Update and rename text2sppech.py to text2speech.py

---
+++ text2speech.py
@@ -0,0 +1,6 @@
+import os
+import pyttsx3
+input = input("Input text you want to covert to speech\n")# input desired text
+pyttsx3.speak(f'{input}') #
+print("complete")#
+#input text and a voice will appear
--- text2sppech.py
+++
@@ -1,6 +0,0 @@
-import os
-import pyttsx3
-input = input("Input text you want to covert to speech\n")
-pyttsx3.speak(f' {input}')
-print("complete")
-#input text and a voice will appear
